4aad9574cbc288f7bcfa2e6e9bb0ee03b11f9231
PPUR22 <160172150+PPUR22@users.noreply.github.com>
2024-02-15 19:37:46 +0000

    Update hallo.cs

--- hallo.cs
+++ hallo.cs
@@ -7,13 +7,13 @@ public class hallo : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        print("Hello World!");
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        print("Hello Mudafuckers!");
         
     }
 }
